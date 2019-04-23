a140311ae3e60e525ec2f8a843c9504bc572401b
JohnHong <nhozstar0024@gmail.com>
2019-04-23 09:18:42 +0700

    220-change

--- 220/220-content.php
+++ 220/220-content.php
@@ -15,14 +15,14 @@ $url_path = str_replace('\\', '/', $url_path);
                 <div class="container">
                     <div class="row">
                         <div class="top-header">
-                            <div class="col-md-4 col-sm-4 col-xs-5">
+                            <div class="col-md-4 col-sm-4 col-xs-4">
                                 <div class="lms_any_que_wrapper">
                                     <a>My Account</a>
                                     <a>Wish</a>
                                     <a>Checkout</a>                                    
                                 </div>
                             </div>
-                            <div class="col-md-4 col-sm-4 col-xs-5">
+                            <div class="col-md-4 col-sm-4 col-xs-4">
                                 <div class="logo">
                                     <img src="<?php echo $url_path ?>/images/rapha.png" alt=""/>
                                 </div>
@@ -44,48 +44,46 @@ $url_path = str_replace('\\', '/', $url_path);
             <div class="clearfix"></div>
             <!--Menu-->
             <div class="navbar-wrapper">
-                <div class="container-fluid">
-                    <nav class="navbar navbar-fixed-top">
-                        <div class="container">
-                            <div class="navbar-header">
-                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                                    <span class="sr-only">Toggle navigation</span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                    <span class="icon-bar"></span>
-                                </button>
-                            </div>
-                            <div id="navbar" class="navbar-collapse collapse">
-                                <ul class="nav navbar-nav">
-                                    <li class="active"><a href="#" class="">Home</a></li>
-                                    <li class=" dropdown">
-                                        <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product<span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="#">Add New</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blogs<span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="#">Team</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages<span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="#">Team</a></li>
-                                            <li><a href="#">Team Single</a></li>
-                                            <li><a href="#">Services</a></li>
-                                            <li><a href="#">Careers</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
-                                    </li>
-                                    <li class=" down"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact US <span class="caret"></span></a>
-                                    </li>
-                                </ul>
-                            </div>
+                <nav class="navbar navbar-fixed-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
                         </div>
-                    </nav>
-                </div>
+                        <div id="navbar" class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#" class="">Home</a></li>
+                                <li class=" dropdown">
+                                    <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Add New</a></li>
+                                    </ul>
+                                </li>
+                                <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blogs<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Team</a></li>
+                                    </ul>
+                                </li>
+                                <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages<span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Team</a></li>
+                                        <li><a href="#">Team Single</a></li>
+                                        <li><a href="#">Services</a></li>
+                                        <li><a href="#">Careers</a></li>
+                                    </ul>
+                                </li>
+                                <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
+                                </li>
+                                <li class=" down"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact US <span class="caret"></span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
             <!--End Menu-->
 
